42dd5b9d508481e1f372d0f4f3ec108e2bd84b09
minhphonghp2003 <minhphonghp2003@gmail.com>
2023-11-09 21:44:07 +0700

    tinymce

--- app/dashboard/new/post/page.js
+++ app/dashboard/new/post/page.js
@@ -1,53 +1,50 @@
 "use client";
 import React, { useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
+import ReactHtmlParser from 'react-html-parser';
 
-function TextEditor() {
-    const [value, setValue] = useState("");
+function TextEditor({onPost}) {
     const editorRef = useRef(null);
-    const submit = () => {
+    let handleSubmit = () => {
         if (editorRef.current) {
-            console.log(editorRef.current.getContent());
+            let content = editorRef.current.getContent();
+            onPost(content)
         }
     };
     return (
         <>
-            {" "}
             <button
-                onClick={submit}
+                onClick={handleSubmit}
                 className="btn btn-xs h-[3rem] w-[10rem] bg-[#696cff] text-white"
             >
                 Post
-            </button>
+            </button>{" "}
             <Editor
+                onInit={(evt, editor) => editorRef.current = editor}
+                id="editor"
                 apiKey="sncffu26ys9pgaa4fp1ozl0g80ttdu6nv00yodyd8zgccgfv"
                 init={{
-                  selector: 'textarea',
+                    selector: "textarea",
                     plugins:
-                        "ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+                        "preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion",
                     toolbar:
-                        "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
-                    tinycomments_mode: "embedded",
-                    tinycomments_author: "Author name",
-                    mergetags_list: [
-                        { value: "First.Name", title: "First Name" },
-                        { value: "Email", title: "Email" },
-                    ],
-                    ai_request: (request, respondWith) =>
-                        respondWith.string(() =>
-                            Promise.reject("See docs to implement AI Assistant")
-                        ),
+                        "undo redo | accordion accordionremove | blocks fontfamily fontsize | bold italic underline strikethrough | align numlist bullist | link image | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample | ltr rtl",
                 }}
-                initialValue="Welcome to TinyMCE!"
             />
         </>
     );
 }
 
 function Post() {
+    let [content,setContent]= useState("")
+    let handlePost = (data) => {
+        console.log(data);
+        setContent(data)
+    };
     return (
         <div className="mt-10 flex flex-col gap-2 items-center">
-            <TextEditor />
+            <TextEditor onPost={handlePost} />
+             {/* <div>{ ReactHtmlParser(content) }</div> */}
         </div>
     );
 }
